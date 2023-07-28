09e66813b7d1bc7a22bf471272fd1b3542a5558b
Dev Raj Singh <55313450+devraj4522@users.noreply.github.com>
2023-07-29 05:20:37 +0530

    Update SideMenu.jsx

--- src/components/Layout/SideMenu.jsx
+++ src/components/Layout/SideMenu.jsx
@@ -24,7 +24,7 @@ const SideMenu = ({ currentUser, handleLogout }) => {
     <div className="stickyCol">
       <Grid>
         <MenuRow href="/" menuName="Home" iconName="home" />
-        <MenuRow href="https://social-media-api-git-main-devraj4522.vercel.app/" menuName="Social" iconName="react" />
+        <MenuRow href="https://freelance-site-lake.vercel.app" menuName="Social" iconName="react" />
         {/* <MenuRow only="mobile tablet" iconName="search" href="/search" /> */}
         {currentUser && (
           <>
