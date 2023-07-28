7793ef97e73b4634b9049f44c59839be7a4c8a28
Dev Raj Singh <55313450+devraj4522@users.noreply.github.com>
2023-07-29 01:59:29 +0530

    Update newRequest.js

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
   baseURL: "https://freelance-api-9qh1.onrender.com/api",
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 export default newRequest;
